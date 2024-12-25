f4f5c28d3ec24f9a492b979b3bb915d26522ebe5
Oleksandr <sidrsidr2@gmail.com>
2024-12-25 22:21:03 +0200

    final clean vers

--- src/App.js
+++ src/App.js
@@ -336,27 +336,6 @@ function MovieDetails({ selectedId, onCloseMovie, handleAddWatched, watched }) {
     </div>
   );
 }
-// function WatchedBox() {
-//   const [watched, setWatched] = useState(tempWatchedData);
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2((open) => !open)}
-//       >
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-//           <WatchedSummary watched={watched} />
-//           <WatchedMovieList watched={watched} />
-//         </>
-//       )}
-//     </div>
-//   );
-// }
 
 function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
